118671e5997d93a0b39f8f9451f402752a3e977e
Rohan Taneja <rohan.taneja@biz2credit.org>
2021-07-31 20:03:07 +0530

    add database encryption part in app.js for authentication(Level2)

--- app.js
+++ app.js
@@ -1,1 +1,86 @@
-//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// --------Normal way to generate schema--------
+// const userSchema = {
+//   email: String,
+//   password: String,
+// };
+// --------End of Normal way to generate schema--------
+
+// --------Generate schema with Mongoose Encryption--------
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+// --------End of Generate schema with Mongoose Encryption--------
+
+//Always use encrypt method before the mongoose model
+
+const secret = 'Thisisoursecret';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    if (!err) {
+      res.render('secrets');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (!err) {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets');
+        }
+      }
+    } else {
+    }
+  });
+});
+
+app.listen('3000', () => {
+  console.log('Server started at port 3000');
+});
